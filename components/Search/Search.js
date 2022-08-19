ee1d8b3f44be2a2d421a196ab110ce9f9d53074a
Kamal Bilal <kamalbilalhuawei@gmail.com>
2022-08-19 22:50:10 +0500

    added search but not completed yet

--- components/Search/Search.js
+++ components/Search/Search.js
@@ -1,10 +1,38 @@
 import styles from "./Search.module.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function Search({ title, page }) {
+  const [searchedData, setSearchedData] = useState();
+  async function getSearchedProducts() {
+    let options = {
+      url: "http://localhost:8000/getsearchedproducts",
+      method: "POST",
+      credentials: "include",
+      withCredentials: true,
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      data: {
+        title: title,
+        pwd: "Kamal",
+      },
+    };
+
+    const response = await axios(options).catch((error) => console.log(error));
+    if (!response) return console.log("response error");
+    if (response.data.success === true) {
+      setSearchedData(response.data.products);
+    }
+  }
+
   useEffect(() => {
-    console.log(page);
+    getSearchedProducts();
   }, []);
+  useEffect(() => {
+    console.log(searchedData);
+  }, [searchedData]);
 
   return (
     <div className={styles.body}>
